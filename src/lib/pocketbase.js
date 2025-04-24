e30993a54c540c6993226561a5241a823216cef2
klavijor <klavijorangel@gmail.com>
2025-04-23 19:19:00 -0500

    change pocketbase url

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,7 +1,8 @@
 import PocketBase from 'pocketbase'
 
 // Usa esta URL si estás en local
-const pb = new PocketBase('http://127.0.0.1:8090')
+/* const pb = new PocketBase('http://127.0.0.1:8090') */
+const pb = new PocketBase('https://backend-alejisa.onrender.com')
 
 // Opcional: puedes activar logging de peticiones/respuestas
 // pb.autoCancellation(false)
